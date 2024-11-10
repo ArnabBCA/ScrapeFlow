190f5a56649d620a4219f982dd29bb6e6511534a
Arnab Ghosh <arnabsonu2@gmail.com>
2024-11-10 22:10:19 +0530

    Added Workflow Card

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -4,6 +4,7 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AlertCircle, InboxIcon } from "lucide-react";
 import React, { Suspense } from "react";
+import WorkflowCard from "./_components/WorkflowCard";
 
 const page = () => {
   return (
@@ -65,7 +66,13 @@ async function UserWorkflows() {
     );
   }
 
-  return <div>er</div>;
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {workflows.map((workflow) => (
+        <WorkflowCard key={workflow.id} workflow={workflow} />
+      ))}
+    </div>
+  );
 }
 
 export default page;
