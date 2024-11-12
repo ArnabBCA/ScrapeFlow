1e0e6f93364710a760e12b9f9e4805f40cf341cb
Arnab Ghosh <arnabsonu2@gmail.com>
2024-11-12 14:27:16 +0530

    Update Connections validation

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -96,6 +96,53 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
     [setEdges, updateNodeData, nodes]
   );
 
+  const isValidConnection = useCallback(
+    (connection: Edge | Connection) => {
+      // No self-connections allowed
+      if (connection.source === connection.target) {
+        return false;
+      }
+
+      // Same taskParam type connection
+      const source = nodes.find((node) => node.id === connection.source);
+      const target = nodes.find((node) => node.id === connection.target);
+      if (!source || !target) {
+        console.error("invalid connection: source or target node not found");
+        return false;
+      }
+
+      const sourceTask = TaskRegistry[source.data.type];
+      const targetTask = TaskRegistry[target.data.type];
+
+      const output = sourceTask.outputs.find(
+        (o) => o.name === connection.sourceHandle
+      );
+
+      const input = targetTask.inputs.find(
+        (o) => o.name === connection.targetHandle
+      );
+
+      if (input?.type !== output?.type) {
+        console.error("invalid connection: type mismatch");
+        return false;
+      }
+
+      const hasCycle = (node: AppNode, visited = new Set()) => {
+        if (visited.has(node.id)) return false;
+        visited.add(node.id);
+
+        for (const outgoer of getOutgoers(node, nodes, edges)) {
+          if (outgoer.id === connection.source) return true;
+          if (hasCycle(outgoer, visited)) return true;
+        }
+      };
+
+      const detectedCycle = hasCycle(target);
+      return !detectedCycle;
+    },
+    [nodes, edges]
+  );
+
   return (
     <main className="h-full w-full">
       <ReactFlow
@@ -112,6 +159,7 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
         onDragOver={onDragOver}
         onDrop={onDrop}
         onConnect={onConnect}
+        isValidConnection={isValidConnection}
       >
         <Controls position="top-left" fitViewOptions={fitViewOptions} />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
