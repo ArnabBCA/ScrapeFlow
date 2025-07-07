b6e212a0aa4b4207b6476e0d8a2301209c5d99c4
Arnab Ghosh <arnabsonu2@gmail.com>
2025-07-07 21:36:58 +0530

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@sparticuz/chromium", "puppeteer-core");
+    }
+    return config;
   },
 };
 
