29a9095adfd386550d7f4515a806ea9a47ef1137
Arnab Ghosh <arnabsonu2@gmail.com>
2025-07-07 22:16:54 +0530

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
   },
-  webpack(config) {
-    config.externalsPresets = { node: true };
-    config.externals = {
-      ...config.externals,
-      "@sparticuz/chromium": "commonjs2 @sparticuz/chromium",
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
