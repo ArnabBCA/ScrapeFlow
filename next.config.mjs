eb37382a711680c3c8bacab7989f304a1af9d9fe
Arnab Ghosh <arnabsonu2@gmail.com>
2025-07-08 02:15:02 +0530

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, options) => {
-    if (!config.externals) {
-      config.externals = [];
-    }
-
-    config.externals = [
-      function (context, request, callback) {
-        if (/^@sparticuz\/chromium-min/.test(request)) {
-          return callback(null, "@sparticuz/chromium-min");
-        }
-        callback();
-      },
-      ...config.externals,
-    ];
-
-    return config;
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium"],
   },
 };
 
