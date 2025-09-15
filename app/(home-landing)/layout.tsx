3f783396275821b5600492baf66e9990137901a0
Arnab Ghosh <arnabsonu2@gmail.com>
2025-09-15 12:40:48 +0530

    changed landing page

---
+++ app/(home-landing)/layout.tsx
@@ -0,0 +1,5 @@
+import { GridBackground } from "./_components/GridBackground";
+
+export default function ({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
--- app/(home-landing)/page.tsx
+++ app/(home-landing)/page.tsx
@@ -10,34 +10,67 @@ import { FeaturesGradient } from "./_components/FeaturesGradient";
 import { HoverEffect } from "@/components/accernity-ui/CardHover";
 import Link from "next/link";
 import Navbar from "./_components/Navbar";
+import { Lamp } from "./_components/Lamp";
+import Typewriter from "typewriter-effect";
 
 export default function HomeLandingPage() {
   return (
-    <div className="flex flex-col min-h-screen gap-4 selection:bg-primary selection:text-white dark bg-[#0C0A09] ">
+    <div className="flex flex-col items-center min-h-screen selection:bg-primary selection:text-white">
       <Navbar />
-      <SectionWrapper className="h-[35rem] text-center">
-        <TypewriterEffectSmooth
-          words={typeWriterWords}
-          className="mb-0 space-y-0"
-          cursorClassName="bg-primary"
-        />
-        <p className="text-muted-foreground text-sm md:text-xl">
-          Create, automate, and scale your web scraping projects with ease. No
-          coding required.
-        </p>
+      <SectionWrapper className="h-screen text-center -mt-[80px] !p-0">
+        <Lamp>
+          <div className="flex flex-col items-center gap-4">
+            {/*<TypewriterEffectSmooth
+              words={typeWriterWords}
+              className="mb-0"
+              cursorClassName="bg-primary"
+            />*/}
+            <Typewriter
+              options={{
+                strings: [
+                  '<span>Build Powerful <span style="color: #27ae60;">Web Scraping</span> Workflows.</span>',
+                ],
+                autoStart: true,
+                loop: true,
+                skipAddStyles: true,
 
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-          <Button
-            className="w-40 h-10 rounded-xl text-sm border-primary text-primary hover:text-white hover:bg-primary"
-            variant={"outline"}
-          >
-            <Link href={"/sign-in"}>Get Started</Link>
-            <ChevronRightIcon className="h-4 w-4" />
-          </Button>
-        </div>
-        <p className="text-sm text-primary">
-          New users get 200 credits for free upon first login
-        </p>
+                cursorClassName:
+                  "text-primary text-2xl md:text-3xl lg:text-4xl xl:text-5xl blinking-cursor",
+                wrapperClassName:
+                  "text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center",
+              }}
+            />
+            <p className="text-muted-foreground text-sm md:text-xl">
+              Create, automate, and scale your web scraping projects with ease.
+              No coding required.
+            </p>
+          </div>
+
+          <div className="relative rounded-full p-[1px] h-8 inline-flex overflow-hidden text-[14px]/6 text-gray-200 transition my-10">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FFFF00_0%,transparent_50%,#FFFF00_100%)]"></span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex h-full px-3 py-1 w-full cursor-pointer items-center justify-center rounded-full bg-stone-900 backdrop-blur-3xl"
+              aria-label="Read about our latest undetectability update"
+              href="/still_working"
+            >
+              Powered by
+              <span className="ml-1 font-semibold text-[#FFFF00]">AI</span>
+            </a>
+          </div>
+          <div className="flex flex-col items-center gap-4">
+            <Link href={"/sign-in"}>
+              <Button className="w-40 h-10 rounded-xl text-sm text-white shadow-[0_0_10px_4px] shadow-green-500/50">
+                Get Started
+                <ChevronRightIcon className="h-4 w-4" />
+              </Button>
+            </Link>
+            <p className="text-sm text-primary">
+              New users get 200 credits for free upon first login
+            </p>
+          </div>
+        </Lamp>
       </SectionWrapper>
       <SectionWrapper
         id="howItWorks"
@@ -55,7 +88,6 @@ export default function HomeLandingPage() {
       </SectionWrapper>
       <SectionWrapper
         id="pricing"
-        className="w-full py-12 md:py-24 lg:py-32"
         primaryTitle="Simple"
         secondaryTitle="Pricing"
       >
@@ -103,15 +135,16 @@ function SectionWrapper({
   return (
     <section
       className={cn(
-        "flex flex-col items-center justify-center gap-4 py-10 box-border max-w-screen-xl mx-auto scroll-mt-[80px] p-5 md:p-10",
+        "flex flex-col items-center justify-center py-10 box-border w-full max-w-screen-xl scroll-mt-[80px] md:p-10",
         className
       )}
       id={id}
     >
-      <div className="text-2xl md:text-4xl lg:text-6xl text-foreground">
-        <span className="text-primary">{primaryTitle}</span>{" "}
-        <span className="">{secondaryTitle}</span>
-      </div>
+      {primaryTitle && (
+        <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-3xl lg:text-5xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          {primaryTitle} {secondaryTitle}
+        </h1>
+      )}
       {children}
     </section>
   );
