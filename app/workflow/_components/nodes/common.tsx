f22aa3d48f05a65db08a092c1ea08f486052a406
Arnab Ghosh <arnabsonu2@gmail.com>
2025-10-06 22:37:59 +0530

    changed common CREDENTIAL color

--- app/workflow/_components/nodes/common.tsx
+++ app/workflow/_components/nodes/common.tsx
@@ -4,5 +4,5 @@ export const ColorForHandle: Record<TaskParamType, string> = {
   BROWSE_INSTANCE: "!bg-sky-400",
   STRING: "!bg-amber-400",
   SELECT: "!bg-rose-400",
-  CREDENTIAL: "!bg-tea-400",
+  CREDENTIAL: "!bg-purple-400",
 };
